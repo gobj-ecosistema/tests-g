1e5454bdace1cc5a97519c8e3956859146acc186
Niyamaka <niyamaka@yuneta.io>
2019-10-27 09:34:53 +0100

    scripts for test

---
+++ test_timeranger.h
@@ -0,0 +1,3 @@
+../output/bin/test_timeranger $1 $2
+../output/bin/test_timeranger2 $1 $2
+../output/bin/test_timeranger3 $1 $2
---
+++ test_tr_msg.h
@@ -0,0 +1,4 @@
+../output/bin/test_tr_msg1 $1 $2
+../output/bin/test_tr_msg2 $1 $2
+../output/bin/test_tr_msg3 $1 $2
+../output/bin/test_tr_msg4 $1 $2
