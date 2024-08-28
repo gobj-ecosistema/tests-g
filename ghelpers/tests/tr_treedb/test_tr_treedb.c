1f79781a7b4dc7193692cd7d74141f9026007180
rmm <rosa.martinez@artgins.com>
2024-08-28 12:04:59 +0200

    fix memory loss and clean no-used code

--- ghelpers/tests/tr_treedb/test_tr_treedb.c
+++ ghelpers/tests/tr_treedb/test_tr_treedb.c
@@ -219,6 +219,7 @@ PRIVATE int capture_log_write(void* v, int priority, const char* bf, size_t len)
     }
     if(!expected_log_messages) {
         // Avoid log_error in kw_ function
+        JSON_DECREF(msg);
         return -1;
     }
     json_t *expected_msg = kw_get_list_value(expected_log_messages, 0, 0);
